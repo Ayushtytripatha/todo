4ea221f365b15cfee802329974a9c18571be41a4
Aayush Tripathi <49943117+Ayushtytripatha@users.noreply.github.com>
2021-09-22 08:10:39 +0530

    t4
     watch  24

---
+++ lib/features/Presentation/widgets/common.dart
@@ -0,0 +1,16 @@
+import 'package:todo/features/Presentation/widgets/theme/style.dart';
+
+List<dynamic> taskTypeColor = [
+  color1fd204,
+  color4875b3,
+  color6FADE4,
+  colorC80863,
+  colorFBD608,
+];
+List<String> taskTYpeList = [
+  "Personnal",
+  "Work",
+  "Meeting",
+  "Study",
+  "Others",
+];
